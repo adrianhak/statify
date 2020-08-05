95fe387f9aa6e9d7899d25916712b3c17efb8873
Adrian Håkansson <adrian@adrianh.net>
2020-08-06 00:24:15 +0200

    font change

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <Header></Header>
+	<div id="app">
+		<Header></Header>
 		<keep-alive>
-    	<router-view :key="$route.fullPath" class="wrapper"/>
+			<router-view :key="$route.fullPath" class="wrapper"/>
 		</keep-alive>
 		<vue-progress-bar></vue-progress-bar>
-    <Footer></Footer>
-  </div>
+		<Footer></Footer>
+	</div>
 </template>
 
 <script>
@@ -14,8 +14,8 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import {auth} from './firebaseConfig';
 export default {
-  name: 'App',
-  components: { Header, Footer },
+	name: 'App',
+	components: { Header, Footer },
 	created() {
 		this.$router.beforeEach((to, from, next) => {
 			this.$Progress.start();
@@ -29,34 +29,34 @@ export default {
 </script>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-  html, body {
-    height: 100%;
-  }
-  #app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100%;
-		font-family: 'Inter', sans-serif;
-  }
-  .wrapper {
-		height: 100%;
-		flex: 1;
-		display: flex;
-		flex-flow: column;
-    	width: 100%;
-		background-color: #fafafa;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+html, body {
+	height: 100%;
+}
+#app {
+	display: flex;
+	flex-direction: column;
+	min-height: 100%;
+	font-family: 'Roboto', sans-serif;
+}
+.wrapper {
+	height: 100%;
+	flex: 1;
+	display: flex;
+	flex-flow: column;
+	width: 100%;
+	background-color: #fafafa;
+}
+@media only screen and (max-width: 600px) {
+	/* Provides cleaner UX by preventing text selection on scroll and app navigation */
+	body {
+		-webkit-user-select: none;
+		-moz-user-select: -moz-none;
+		-ms-user-select: none;
+		user-select: none;
 	}
-  @media only screen and (max-width: 600px) {
-    /* Provides cleaner UX by preventing text selection on scroll and app navigation */
-    body {
-      -webkit-user-select: none;
-      -moz-user-select: -moz-none;
-      -ms-user-select: none;
-      user-select: none;
-    }
-    .wrapper {
-        width: 100%;
-    }
-  }
+	.wrapper {
+		width: 100%;
+	}
+}
 </style>
