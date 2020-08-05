4c9181f877823ea733c86cfb2826d78025d193ae
Adrian Håkansson <adrian@adrianh.net>
2020-08-05 18:04:33 +0200

    configure gh-pages hosting

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,39 +1,40 @@
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-	productionSourceMap: false,
-	configureWebpack: {
-		devtool: 'source-map',
-		resolve: {
-			alias: {
-				vue$: process.env.VUE_APP_DIST 
-			}
-		},
-		optimization: {
+		productionSourceMap: false,
+		publicPath: process.env.NODE_ENV == 'production' ? '/statify/' : '/',
+		configureWebpack: {
+				devtool: 'source-map',
+				resolve: {
+						alias: {
+								vue$: process.env.VUE_APP_DIST 
+						}
+				},
+				optimization: {
 
-			splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-				cacheGroups: {
-        	node_vendors: {
-          	test: /[\\/]node_modules[\\/]/,
-            chunks: "all",
-            priority: 1
-          }
-        }
-      },
-			minimizer: [
-				new TerserPlugin({
-					terserOptions: {
-						warnings: false,
-						compress: { drop_console: true },
-						output: { comments: false },
-					},
-					parallel: true,
-					extractComments: false,
-					sourceMap: process.env.NODE_ENV == 'development'
-				})
-			]
+						splitChunks: {
+								minSize: 10000,
+								maxSize: 250000,
+								cacheGroups: {
+										node_vendors: {
+												test: /[\\/]node_modules[\\/]/,
+														chunks: "all",
+														priority: 1
+												}
+								}
+						},
+						minimizer: [
+								new TerserPlugin({
+										terserOptions: {
+												warnings: false,
+												compress: { drop_console: true },
+												output: { comments: false },
+										},
+										parallel: true,
+										extractComments: false,
+										sourceMap: process.env.NODE_ENV == 'development'
+								})
+						]
+				}
 		}
-	}
 }
